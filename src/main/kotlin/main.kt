766b44b6c97e8cbf86a0126deaa86dc03ffb0cca
antonkazakov <antonkazakov@localhost>
2022-01-31 14:35:19 +0300

    fdsf

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,17 @@
+import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.util.FileUtils
 import java.io.File
 
 
 fun main(args: Array<String>) {
-    val repoDir = File("..")
+    val repoDir = File(".")
 
     val builder = FileRepositoryBuilder()
-    val repo = builder.setGitDir(repoDir)
-        .readEnvironment()
-        .findGitDir()
-        .build()
+    val git = Git.open(repoDir)
 
-    repo.let { repository ->
-        val head = repository.exactRef("refs/heads/master")
-        println("Ref of refs/heads/master: $head")
-    }
+    git.add().addFilepattern(".").call()
+
+    git.commit().setMessage("fdsf").call()
+
 }
